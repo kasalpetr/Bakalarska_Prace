5b2e45386656fd19de64a1de04eb79f21720cf0d
Kasal <kasal@Kasal.Kasal>
2026-05-01 17:02:50 +0200

    new potos

--- build/CMakeFiles/DigitalBoard.dir/DependInfo.cmake
+++ build/CMakeFiles/DigitalBoard.dir/DependInfo.cmake
@@ -8,20 +8,25 @@ set(CMAKE_DEPENDS_LANGUAGES
 
 # The set of dependency files which are needed:
 set(CMAKE_DEPENDS_DEPENDENCY_FILES
-  "/mnt/c/FIT CVUT/bakalarka/Bakalarska_Prace/App_W2B/src/Detector/Detector.cpp" "CMakeFiles/DigitalBoard.dir/src/Detector/Detector.cpp.o" "gcc" "CMakeFiles/DigitalBoard.dir/src/Detector/Detector.cpp.o.d"
-  "/mnt/c/FIT CVUT/bakalarka/Bakalarska_Prace/App_W2B/src/Detector/GeometryUtils.cpp" "CMakeFiles/DigitalBoard.dir/src/Detector/GeometryUtils.cpp.o" "gcc" "CMakeFiles/DigitalBoard.dir/src/Detector/GeometryUtils.cpp.o.d"
-  "/mnt/c/FIT CVUT/bakalarka/Bakalarska_Prace/App_W2B/src/Detector/ShapeDetector/CircleDetector.cpp" "CMakeFiles/DigitalBoard.dir/src/Detector/ShapeDetector/CircleDetector.cpp.o" "gcc" "CMakeFiles/DigitalBoard.dir/src/Detector/ShapeDetector/CircleDetector.cpp.o.d"
-  "/mnt/c/FIT CVUT/bakalarka/Bakalarska_Prace/App_W2B/src/Detector/ShapeDetector/EdgeDetector.cpp" "CMakeFiles/DigitalBoard.dir/src/Detector/ShapeDetector/EdgeDetector.cpp.o" "gcc" "CMakeFiles/DigitalBoard.dir/src/Detector/ShapeDetector/EdgeDetector.cpp.o.d"
-  "/mnt/c/FIT CVUT/bakalarka/Bakalarska_Prace/App_W2B/src/Detector/ShapeDetector/SquareDetector.cpp" "CMakeFiles/DigitalBoard.dir/src/Detector/ShapeDetector/SquareDetector.cpp.o" "gcc" "CMakeFiles/DigitalBoard.dir/src/Detector/ShapeDetector/SquareDetector.cpp.o.d"
-  "/mnt/c/FIT CVUT/bakalarka/Bakalarska_Prace/App_W2B/src/Detector/ShapeDetector/TriangleDetector.cpp" "CMakeFiles/DigitalBoard.dir/src/Detector/ShapeDetector/TriangleDetector.cpp.o" "gcc" "CMakeFiles/DigitalBoard.dir/src/Detector/ShapeDetector/TriangleDetector.cpp.o.d"
-  "/mnt/c/FIT CVUT/bakalarka/Bakalarska_Prace/App_W2B/src/ImageWork/ImageLoader.cpp" "CMakeFiles/DigitalBoard.dir/src/ImageWork/ImageLoader.cpp.o" "gcc" "CMakeFiles/DigitalBoard.dir/src/ImageWork/ImageLoader.cpp.o.d"
-  "/mnt/c/FIT CVUT/bakalarka/Bakalarska_Prace/App_W2B/src/ImageWork/ImageMask.cpp" "CMakeFiles/DigitalBoard.dir/src/ImageWork/ImageMask.cpp.o" "gcc" "CMakeFiles/DigitalBoard.dir/src/ImageWork/ImageMask.cpp.o.d"
-  "/mnt/c/FIT CVUT/bakalarka/Bakalarska_Prace/App_W2B/src/JsonExport/JsonExporter.cpp" "CMakeFiles/DigitalBoard.dir/src/JsonExport/JsonExporter.cpp.o" "gcc" "CMakeFiles/DigitalBoard.dir/src/JsonExport/JsonExporter.cpp.o.d"
-  "/mnt/c/FIT CVUT/bakalarka/Bakalarska_Prace/App_W2B/src/Main.cpp" "CMakeFiles/DigitalBoard.dir/src/Main.cpp.o" "gcc" "CMakeFiles/DigitalBoard.dir/src/Main.cpp.o.d"
+  "/home/kasal/Bakalarka/Bakalarska_Prace/App_W2B/src/Detector/Detector.cpp" "CMakeFiles/DigitalBoard.dir/src/Detector/Detector.cpp.o" "gcc" "CMakeFiles/DigitalBoard.dir/src/Detector/Detector.cpp.o.d"
+  "/home/kasal/Bakalarka/Bakalarska_Prace/App_W2B/src/Detector/GeometryUtils.cpp" "CMakeFiles/DigitalBoard.dir/src/Detector/GeometryUtils.cpp.o" "gcc" "CMakeFiles/DigitalBoard.dir/src/Detector/GeometryUtils.cpp.o.d"
+  "/home/kasal/Bakalarka/Bakalarska_Prace/App_W2B/src/Detector/ShapeDetector/CircleDetector.cpp" "CMakeFiles/DigitalBoard.dir/src/Detector/ShapeDetector/CircleDetector.cpp.o" "gcc" "CMakeFiles/DigitalBoard.dir/src/Detector/ShapeDetector/CircleDetector.cpp.o.d"
+  "/home/kasal/Bakalarka/Bakalarska_Prace/App_W2B/src/Detector/ShapeDetector/EdgeDetector.cpp" "CMakeFiles/DigitalBoard.dir/src/Detector/ShapeDetector/EdgeDetector.cpp.o" "gcc" "CMakeFiles/DigitalBoard.dir/src/Detector/ShapeDetector/EdgeDetector.cpp.o.d"
+  "/home/kasal/Bakalarka/Bakalarska_Prace/App_W2B/src/Detector/ShapeDetector/SquareDetector.cpp" "CMakeFiles/DigitalBoard.dir/src/Detector/ShapeDetector/SquareDetector.cpp.o" "gcc" "CMakeFiles/DigitalBoard.dir/src/Detector/ShapeDetector/SquareDetector.cpp.o.d"
+  "/home/kasal/Bakalarka/Bakalarska_Prace/App_W2B/src/Detector/ShapeDetector/TriangleDetector.cpp" "CMakeFiles/DigitalBoard.dir/src/Detector/ShapeDetector/TriangleDetector.cpp.o" "gcc" "CMakeFiles/DigitalBoard.dir/src/Detector/ShapeDetector/TriangleDetector.cpp.o.d"
+  "/home/kasal/Bakalarka/Bakalarska_Prace/App_W2B/src/ImageWork/ImageLoader.cpp" "CMakeFiles/DigitalBoard.dir/src/ImageWork/ImageLoader.cpp.o" "gcc" "CMakeFiles/DigitalBoard.dir/src/ImageWork/ImageLoader.cpp.o.d"
+  "/home/kasal/Bakalarka/Bakalarska_Prace/App_W2B/src/ImageWork/ImageMask.cpp" "CMakeFiles/DigitalBoard.dir/src/ImageWork/ImageMask.cpp.o" "gcc" "CMakeFiles/DigitalBoard.dir/src/ImageWork/ImageMask.cpp.o.d"
+  "/home/kasal/Bakalarka/Bakalarska_Prace/App_W2B/src/JsonExport/JsonExporter.cpp" "CMakeFiles/DigitalBoard.dir/src/JsonExport/JsonExporter.cpp.o" "gcc" "CMakeFiles/DigitalBoard.dir/src/JsonExport/JsonExporter.cpp.o.d"
+  "/home/kasal/Bakalarka/Bakalarska_Prace/App_W2B/src/Main.cpp" "CMakeFiles/DigitalBoard.dir/src/Main.cpp.o" "gcc" "CMakeFiles/DigitalBoard.dir/src/Main.cpp.o.d"
+  "" "DigitalBoard" "gcc" "CMakeFiles/DigitalBoard.dir/link.d"
   )
 
-# Targets to which this target links.
-set(CMAKE_TARGET_LINKED_INFO_FILES
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
   )
 
 # Fortran module output directory.
