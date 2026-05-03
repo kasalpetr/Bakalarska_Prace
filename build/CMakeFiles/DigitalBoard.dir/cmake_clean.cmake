2b4caa79ed6df827fbf1a94ab72fa0daf2f90f72
Kasal <kasal@Kasal.Kasal>
2026-05-03 21:21:45 +0200

    new table

--- build/CMakeFiles/DigitalBoard.dir/cmake_clean.cmake
+++ build/CMakeFiles/DigitalBoard.dir/cmake_clean.cmake
@@ -1,4 +1,5 @@
 file(REMOVE_RECURSE
+  "CMakeFiles/DigitalBoard.dir/link.d"
   "CMakeFiles/DigitalBoard.dir/src/Detector/Detector.cpp.o"
   "CMakeFiles/DigitalBoard.dir/src/Detector/Detector.cpp.o.d"
   "CMakeFiles/DigitalBoard.dir/src/Detector/GeometryUtils.cpp.o"
